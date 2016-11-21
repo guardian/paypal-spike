1d6373a386db174d6bc788f76d3b5e74c633717a
Jamie Byers <james.byers@guardian.co.uk>
2016-11-21 17:48:32 +0000

    Added function for setting up a payment and retrieving the token.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # ----- Imports ----- #
 
+from urllib.parse import parse_qs
 import requests
 
 
@@ -21,7 +22,24 @@ def build_data(data):
 
 	"""Formats the data to be sent in a request, adds required defaults."""
 
-	return {REQUEST_DEFAULTS**, data**}
+	return {**REQUEST_DEFAULTS, **data}
 
 
+def setup_payment():
 
+	"""Sets up the payment authorization, returns the token."""
+
+	req_data = {
+		'METHOD': 'SetExpressCheckout',
+		'PAYMENTREQUEST_0_PAYMENTACTION': 'SALE',
+		'PAYMENTREQUEST_0_AMT': '4.20',
+		'PAYMENTREQUEST_0_CURRENCYCODE': 'GBP',
+		'RETURNURL': 'http://localhost:9000/',
+		'CANCELURL': 'http://localhost:9000/',
+		'BILLINGTYPE': 'MerchantInitiatedBilling'
+	}
+
+	r = requests.post(ENPOINT_URL, data=build_data(req_data))
+	response_data = parse_qs(r.text)
+
+	return response_data['TOKEN']
