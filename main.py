383effdafd20367b04a1c025985d83f3210e5d93
Jamie Byers <james.byers@guardian.co.uk>
2016-11-21 17:17:56 +0000

    Added function to build the default request data, and add any new data required.

--- main.py
+++ main.py
@@ -6,3 +6,22 @@ import requests
 # ----- Setup ----- #
 
 ENPOINT_URL = 'https://api-3t.sandbox.paypal.com/nvp'
+
+REQUEST_DEFAULTS = {
+	'USER': '***REMOVED***',
+	'PWD': '***REMOVED***',
+	'SIGNATURE': '***REMOVED***',
+	'VERSION': '86.0'
+}
+
+
+# ----- Functions ----- #
+
+def build_data(data):
+
+	"""Formats the data to be sent in a request, adds required defaults."""
+
+	return {REQUEST_DEFAULTS**, data**}
+
+
+
